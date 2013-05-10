7d730f065ac7d63e242e84d53e1725edafb238c6
JiniaTS <jmenoprimeni221@gmail.com>
2013-05-10 12:44:13 +0200

    Update boss_corbosus.cpp

--- boss_corbosus.cpp
+++ boss_corbosus.cpp
@@ -1,6 +1,7 @@
 + SPELL_RAGE    = 80158,
 + SPELL_CLEAVE  = 15496,
 + SPELL_DISGUE  = 121308,
++ SPELL_FLAY    = 79922,
 
 class stonecore_warbringer : public CreatureScript
 {
@@ -51,3 +52,44 @@ DoMeleeAttackIfReady();
 }
 };
 };
+
+class stonecore_flayer : public CreatureScript
+{
+  public:
+  
+  stonecore_flayer() : CreatureScript("stonecore_flayer") {}
+  
+  CreatureAI* GetAI(Creature* creature) const
+  {
+    return new stonecore_flayerAI(creature);
+  }
+  
+  struct stonecore_flayerAI : public ScriptedAI
+  {
+    stonecore_flayerAI(Creature* creature) : ScriptesAI(creature)
+    {
+      me->SetUInt32Value(UNIT_NPC_EMOTESTATE, EMOTE_STATE_USESTANDING)
+    }
+    
+    uint32 flaytimer;
+    
+    void Reset()
+    {
+      flaytimer = 25000;
+    }
+    
+    void UpdateAI(uint32 diff)
+    {
+      if(flaytimer <= diff)
+      {
+        DoCast(me->getVictim(), SPELL_FLAY);
+        flaytimer = 25000;
+      }else flaytimer -= diff;
+      
+      DoMeleeAttackIfReady();
+    }
+};
+};
+
++ new stonecore_warbringer();
++ new stonecore_flayer();
