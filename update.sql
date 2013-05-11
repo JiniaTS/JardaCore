7a0a1eb71b13ab69ea8e0cb2b19ebb57d384b632
JiniaTS <jmenoprimeni221@gmail.com>
2013-05-11 19:42:41 +0300

    Create update.sql

---
+++ update.sql
@@ -0,0 +1,4 @@
+INSERT INTO smart_scripts(entryorguid, id , event_type, event_chance, event_param3, event_param4, action_type, action_param1, target_type, COMMENT) VALUES
+(34850, 0, 1, 33, 30000, 30000, 1, 0, 1, 'prince liam say1'),
+(34850, 1, 1, 33, 30000, 30000, 1, 1, 1, 'prince liam say2'),
+(34850, 2, 1, 33, 30000, 30000, 1, 2, 1, 'prince liam say3');
