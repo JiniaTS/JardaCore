99f7800f4d2201494810372caba5bcef0cf29f07
JiniaTS <jmenoprimeni221@gmail.com>
2013-06-05 03:06:45 +0300

    Update Elwynn_forest.sql

--- Elwynn_forest.sql
+++ Elwynn_forest.sql
@@ -206,3 +206,8 @@ INSERT INTO creature_involvedrelation(id, quest) VALUES
 (197, 28823); 
 
 DELETE FROM creature_questrelation WHERE id IN(49871, 49874);
+
+UPDATE creature_template SET AIName='SmartAI', unit_flags=131072, faction_A=7, faction_H=7, unit_flags2=2048 WHERE entry=44548;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-168266 AND `id`=0 AND `source_type`=0 LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES (-168266,0,0,0,8,0,100,0,100,0,0,0,33,44548,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"On Spell Hit get quest credit--Warrior");
+UPDATE quest_template SET RequiredNpcOrGo1=44548 WHERE Id=26913;
